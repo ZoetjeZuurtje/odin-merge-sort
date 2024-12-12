4bd5de5116f447f9d50e0ecaa9166a2076c0f788
Bram <bramstrik@live.com>
2024-12-12 11:05:18 +0100

    Adhere to StandardJS style guidelines

--- sort.js
+++ sort.js
@@ -1,33 +1,28 @@
-function mergeSort(array) {
-
+function mergeSort (array) {
   // base case
-  const len = array.length;
-  if (len < 2) return array;
+  const len = array.length
+  if (len === 1) return array
 
   // split
-  const halfwayPoint = Math.floor(len / 2);
-  const firstHalf = mergeSort(array.slice(0, halfwayPoint));
-  const secondHalf = mergeSort(array.slice(halfwayPoint));
+  const halfwayPoint = Math.floor(len / 2)
+  const firstHalf = mergeSort(array.slice(0, halfwayPoint))
+  const secondHalf = mergeSort(array.slice(halfwayPoint))
 
   // merge
-  let sortedArray = [];
+  const sortedArray = []
   for (let i = 0; i < len; i++) {
-    const firstNumber = firstHalf[0];
+    const firstNumber = firstHalf[0]
     const secondNumber = secondHalf[0]
-    if (firstNumber === undefined) {
-      sortedArray.push(secondHalf.shift())
-      continue;
-    }
-    if (secondNumber === undefined) {
-      sortedArray.push(firstHalf.shift())
-      continue;
-    }
 
-    const lowestNumber = firstNumber < secondNumber ? firstHalf.shift() : secondHalf.shift();
-    sortedArray.push(lowestNumber);
+    // If any of these are undefined, it means that the half is empty
+    // Therefore, the other half should be appended to the sorted array.
+    // No need to do any more comparisons, yay!
+    if (firstNumber === undefined) return sortedArray.concat(secondHalf)
+    if (secondNumber === undefined) return sortedArray.concat(firstHalf)
+
+    const lowestNumber = firstNumber < secondNumber ? firstHalf.shift() : secondHalf.shift()
+    sortedArray.push(lowestNumber)
   }
 
-  return sortedArray;
+  return sortedArray
 }
-
-console.log(mergeSort([2, 1, 8 , 29, 13, 248]));
